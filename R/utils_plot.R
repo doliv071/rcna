749ff100a083ae41e5b530c521fef8fddf076dc1
David Oliver <doliv071@gmail.com>
2026-03-12 10:18:37 -0400

    fix bugs I introduced into plotting functions

--- R/utils_plot.R
+++ R/utils_plot.R
@@ -12,15 +12,16 @@
 #' @export 
 dimplot.generic <- function(coords, y, 
                             label = NULL, 
-                            color.low = muted("blue"),
-                            color.high = muted("red")) {
-    plt_df <- cbind(coords, y) |> 
-        as.data.frame() |> 
+                            color.low = "blue",
+                            color.high = "red") {
+    coords <- coords[,1:2]
+    colnames(coords) <- c("Dim1", "Dim2")
+    plt_df <- cbind.data.frame(coords, y = y) |> 
         # shuffle WITHOUT replacement
         dplyr::slice_sample(prop = 1, replace = FALSE) 
-    plt <- ggplot2::ggplot(ggplot2::aes_string(colnames(coords)[1], 
-                                               colnames(coords)[2], 
-                                               color = 'y')) + 
+    plt <- ggplot2::ggplot(plt_df, ggplot2::aes(x = Dim1, 
+                                                y = Dim2, 
+                                                color = y)) + 
         ggplot2::geom_point(size = 0.5) + 
         ggplot2::scale_color_gradient2(low = color.low, 
                                        high = color.high) + 
@@ -36,26 +37,30 @@ dimplot.generic <- function(coords, y,
 #' @param res A result output from [association] function
 #' @param fdr_thresh A numeric FDR threshold (\alpha), values larger than this
 #' are colored gray.
+#' @param color.low,color.high Low and high color for correlation values.\cr
+#' Default: muted("blue") and muted("red"), respectively
 #' 
 #' @returns A ggplot object 
 #' 
 #' @export 
 dimplot.ncorr <- function(coords, res, 
-                          fdr_thresh = 0.05) {
-    .thresh <- subset(res$fdrs, fdr < fdr_thresh)$threshold[1]    
-    plt_df <- cbind(coords, correlation = res$ncorrs) |> 
-        as.data.frame() |> 
+                          fdr_thresh = 0.05,
+                          color.low = "blue",
+                          color.high = "red") {
+    .thresh <- subset(res$fdrs, fdr < fdr_thresh)$threshold[1]
+    coords <- coords[,1:2]
+    colnames(coords) <- c("Dim1", "Dim2")
+    plt_df <- cbind.data.frame(coords, correlation = res$ncorrs) |> 
         dplyr::mutate(passed = abs(correlation) > .thresh) |> 
         dplyr::slice_sample(prop = 1, replace = FALSE)
     
-    plt <- ggplot2::ggplot(plt_df, ggplot2::aes_string(colnames(coords)[1], 
-                                                       colnames(coords)[2])) + 
+    plt <- ggplot2::ggplot(plt_df, ggplot2::aes(x = Dim1, y = Dim2)) + 
         ggplot2::geom_point(data = subset(plt_df, !passed), 
                             size = .5, color = 'grey', alpha = .05) + 
         ggplot2::geom_point(data = subset(plt_df, passed), 
                             ggplot2::aes(color = correlation),
                             size = .5,  alpha = .5) + 
-        ggplot2::scale_color_gradient2(low = muted("blue"), high = muted("red")) + 
+        ggplot2::scale_color_gradient2(low = "blue", high = "red") + 
         ggplot2::theme_classic(base_size = 16)
     return(plt)
 }
